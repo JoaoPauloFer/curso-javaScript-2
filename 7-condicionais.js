05e23dc71cd8341c5b32a9e612c14caddc857155
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-02 10:57:17 -0300

    docs: 📑  Usando if(s) encadeados

---
+++ 7-condicionais.js
@@ -0,0 +1,29 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 17;
+const estaAcompanhado =true
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+if (idadeComprador >= 18) {
+    console.log("Comprador maior de idade");
+    listaDeDestinos.splice(1,1);
+} else if(estaAcompanhado){
+    console.log("Comprador está acompanhado");
+    listaDeDestinos.splice(1,1);
+} else {
+    console.log("Não é maior de idade e não poss o vender");
+}
+
+console.log(listaDeDestinos);
+
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador == 18);
